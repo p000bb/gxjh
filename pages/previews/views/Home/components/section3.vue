405acc37f67d807e1a07201436b19217afdc4484
p000bb <934371212@qq.com>
2023-11-14 17:46:52 +0800

    安装swiper插件

--- pages/previews/views/Home/components/section3.vue
+++ pages/previews/views/Home/components/section3.vue
@@ -20,22 +20,71 @@
         <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
       </div>
     </Transition> -->
+    <div class="overflow-hidden">
+      <div class="w-max">
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+        <image-hover
+          data-hover="imghvr-shutter-in-out-vert"
+          data-aos="zoom-in"
+          class="w-[200px] height-[200px]"
+        ></image-hover>
+      </div>
 
-    <div>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
-      <image-hover data-hover="imghvr-shutter-in-out-vert" data-aos="zoom-in"></image-hover>
+      <swiper :slides-per-view="3" :space-between="50">
+        <swiper-slide>Slide 1</swiper-slide>
+        <swiper-slide>Slide 2</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+        <swiper-slide>Slide 3</swiper-slide>
+      </swiper>
     </div>
   </section>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import { Swiper, SwiperSlide } from "swiper/vue";
+
+// Import Swiper styles
+import "swiper/css";
+
 const sectionRef = ref<HTMLElement | null>(null);
 
 const show = ref<boolean>(true);
