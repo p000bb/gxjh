f6b2dfcc329ee2bec1c989021d3adc6949c80496
p000bb <934371212@qq.com>
2023-12-05 22:50:49 +0800

    提交

--- pages/admin/utils/service.ts
+++ pages/admin/utils/service.ts
@@ -7,6 +7,7 @@ import { getToken, setToken } from "./cache/cookies";
 /** 退出登录并强制刷新页面（会重定向到登录页） */
 function logout() {
   useUserStoreHook().logout();
+  ElMessage.error("登录过期，请重新登录");
   location.reload();
 }
 
